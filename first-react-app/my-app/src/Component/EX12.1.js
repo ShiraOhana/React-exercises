da4405f156d27879705a53f081106a6e45b7129f
ShiraOhana <soshiraohana@gmail.com>
2022-05-22 23:07:05 +0300

    added files

--- first-react-app/my-app/src/Component/EX12.1.js
+++ first-react-app/my-app/src/Component/EX12.1.js
@@ -1,21 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 // import App from "../index";
 // import { render } from "@testing-library/react";
 
 function Ex121() {
-  //   const [jokek, newJoke] = useState(joke);
-  const App = async () => {
+  const [joke, setJoke] = useState(null);
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    getCategories();
+  });
+
+  const getJoke = async () => {
     const response = await axios.get("https://api.chucknorris.io/jokes/random");
     let joke = response.data.value;
-    console.log(joke);
-
-    // this.({ joke: response.data.value });
+    setJoke(joke);
   };
+  const getJokeByCategory = async (category) => {
+    const response = await axios.get(
+      `https://api.chucknorris.io/jokes/random?category=${category}`
+    );
+    let joke = response.data.value;
+    setJoke(joke);
+  };
+  const getCategories = async () => {
+    const response = await axios.get(
+      "https://api.chucknorris.io/jokes/categories"
+    );
+
+    let categories = response.data;
+    setCategories(categories);
+  };
+
   return (
     <div>
-      <button onClick={App}>Jokes</button>
-      <div className="joke">Jokes at the console for now</div>
+      <button onClick={getJoke}>Jokes</button>
+      <div className="joke">{joke}</div>
+      {categories.map((item) => (
+        <button onClick={() => getJokeByCategory(item)}>{item}</button>
+      ))}
     </div>
   );
 }
