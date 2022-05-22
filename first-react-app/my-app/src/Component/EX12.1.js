63a9bc6e9d0dd4f4c30aa69803510f26d9b4bf73
ShiraOhana <soshiraohana@gmail.com>
2022-05-22 13:45:23 +0300

    12.1 class to function

--- first-react-app/my-app/src/Component/EX12.1.js
+++ first-react-app/my-app/src/Component/EX12.1.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 // import App from "../index";
-import { render } from "@testing-library/react";
+// import { render } from "@testing-library/react";
 
 function Ex121() {
+  //   const [jokek, newJoke] = useState(joke);
   const App = async () => {
     const response = await axios.get("https://api.chucknorris.io/jokes/random");
     let joke = response.data.value;
     console.log(joke);
+
+    // this.({ joke: response.data.value });
   };
   return (
     <div>
       <button onClick={App}>Jokes</button>
-      <div></div>
+      <div className="joke">Jokes at the console for now</div>
     </div>
   );
 }
